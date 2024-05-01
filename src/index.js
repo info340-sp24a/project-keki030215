c31181092034259048178b06e30bd8e7a147dec3
keki030215 <keliuhan215@gmail.com>
2024-05-01 14:35:06 -0700

    create components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { MessageItem } from './App.js';
+import { MessageItem } from 'components/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MessageItem />);
